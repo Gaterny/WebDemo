a17e633ed88ebfc7d139fa046f1b4b6656125272
scusong <scusong@foxmail.com>
2018-07-14 14:40:02 +0800

    merge

--- blogproject/comments/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Comment
-
-class CommentForm(forms.ModelForm):
-	class Meta:
-		model = Comment  #表单对应的是Comment类
-		fields = ['name', 'email', 'url', 'text']  #表单需要显示的字段
